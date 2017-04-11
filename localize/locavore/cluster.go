5bd7d288c749d1a8472b6a0a3ce86ef3804e490a
Koby Picker <jkp46@case.edu>
2017-04-11 01:28:38 -0400

    Fixed clustering bug, finished implementing matching
    Signed-off-by: Koby Picker <jkp46@case.edu>

--- localize/locavore/cluster.go
+++ localize/locavore/cluster.go
@@ -5,9 +5,9 @@ import (
 	"log"
 )
 
-func KMedoids(numClusters int, nodes []dgtypes.Clusterable) ([][]dgtypes.Clusterable, []dgtypes.Clusterable) {
-	clusters := make([][]dgtypes.Clusterable, numClusters)
-	medoids := make([]dgtypes.Clusterable, numClusters)
+func KMedoidsFunc(numClusters int, nodes []dgtypes.Clusterable, f func(dgtypes.Clusterable, dgtypes.Clusterable) float64) ([][]dgtypes.Clusterable, []dgtypes.Clusterable) {
+	clusters := make([][]dgtypes.Clusterable, 0, numClusters)
+	medoids := make([]dgtypes.Clusterable, 0, numClusters)
 	if numClusters == 0 || len(nodes) == 0 {
 		log.Printf("No profiles to cluster")
 		return clusters, medoids
@@ -19,70 +19,95 @@ func KMedoids(numClusters int, nodes []dgtypes.Clusterable) ([][]dgtypes.Cluster
 
 	// Initial step: add initial node to the medoids list
 	for i := 0; i < numClusters; i++ {
-		medoids[i] = nodes[i]
+		medoids = append(medoids, nodes[i])
 	}
 	//		And remove the medoids from the nodes list
 	nodes = nodes[numClusters:]
 
 	// Assignment step:
-	clusters = assignToMedoids(medoids, nodes)
-	cost := totalCost(clusters, medoids)
+	clusters = assignToMedoids(medoids, nodes, f)
+	cost := totalCost(clusters, medoids, f)
 
 	// Update step:
+	first := true
 	for newcost := cost - 1; newcost < cost; {
+		//	fmt.Printf("---\n")
+		//	fmt.Printf("Clusters: \n")
+		//	for m := range medoids {
+		//		fmt.Printf("\tMedoid %v:\t Cluster:%v\n\n", medoids[m], clusters[m])
+		//	}
+		//	fmt.Printf("---\n")
+		if first {
+			newcost = cost
+			first = false
+		}
 		cost = newcost
-		updateMedoids(clusters, medoids)
-		newcost = totalCost(clusters, medoids)
+		updateMedoids(clusters, medoids, f)
+		clusters = assignToMedoids(medoids, nodes, f)
+		newcost = totalCost(clusters, medoids, f)
 	}
 
 	return clusters, medoids
 }
 
-func totalCost(clusters [][]dgtypes.Clusterable, medoids []dgtypes.Clusterable) float64 {
+func KMedoids(numClusters int, nodes []dgtypes.Clusterable) ([][]dgtypes.Clusterable, []dgtypes.Clusterable) {
+	return KMedoidsFunc(numClusters, nodes, func(i, j dgtypes.Clusterable) float64 { return i.Dissimilar(j) })
+}
+
+func totalCost(clusters [][]dgtypes.Clusterable, medoids []dgtypes.Clusterable, f func(dgtypes.Clusterable, dgtypes.Clusterable) float64) float64 {
 	var cost float64 = 0
 	for m, cluster := range clusters {
-		cost += clusterCost(medoids[m], cluster)
+		cost += clusterCost(medoids[m], cluster, f)
 	}
 	return cost
 }
 
-func clusterCost(medoid dgtypes.Clusterable, nodes []dgtypes.Clusterable) float64 {
+func clusterCost(medoid dgtypes.Clusterable, nodes []dgtypes.Clusterable, disFunc func(dgtypes.Clusterable, dgtypes.Clusterable) float64) float64 {
 	cost := 0.0
 	for _, node := range nodes {
-		cost += medoid.Dissimilar(node)
+		cost += disFunc(medoid, node)
 	}
 	return cost
 }
 
-func assignToMedoids(medoids []dgtypes.Clusterable, nodes []dgtypes.Clusterable) [][]dgtypes.Clusterable {
+func assignToMedoids(medoids []dgtypes.Clusterable, nodes []dgtypes.Clusterable, disFunc func(dgtypes.Clusterable, dgtypes.Clusterable) float64) [][]dgtypes.Clusterable {
+	//log.Printf("\nAssigning to medoids...")
+	//log.Printf("\nMedoids: [%v]", medoids)
 	clusters := make([][]dgtypes.Clusterable, len(medoids))
 	for cluster := range clusters {
 		clusters[cluster] = make([]dgtypes.Clusterable, 0)
 	}
+	//log.Printf("Created %v clusters: %v", len(clusters), clusters)
 
 	for _, node := range nodes {
 		var nearestMedoid int = 0
-		var minDist float64 = node.Dissimilar(medoids[0])
+		var minDist float64 = disFunc(node, medoids[0])
 		for j := range medoids {
-			dist := node.Dissimilar(medoids[j])
+			dist := disFunc(node, medoids[j])
 			if dist < minDist {
 				minDist = dist
 				nearestMedoid = j
+				//	log.Printf("\t\t\tNot Equal: %v", dist)
+				//} else if dist == minDist {
+				//	log.Printf("\t\t\tEqual: %v", dist)
+				//} else {
+				//	log.Printf("\t\t\tNot Equal: %v", dist)
 			}
 		}
+		//log.Printf("\n\t%v\n is the nearest medoid to \n\t%v", medoids[nearestMedoid], node)
 		clusters[nearestMedoid] = append(clusters[nearestMedoid], node)
 	}
 
 	return clusters
 }
 
-func updateMedoids(clusters [][]dgtypes.Clusterable, medoids []dgtypes.Clusterable) {
+func updateMedoids(clusters [][]dgtypes.Clusterable, medoids []dgtypes.Clusterable, f func(dgtypes.Clusterable, dgtypes.Clusterable) float64) {
 	for m := range medoids {
 		for x := range clusters {
 			for y := range clusters[x] {
-				cost := clusterCost(medoids[m], clusters[m])
+				cost := clusterCost(medoids[m], clusters[m], f)
 				swapCluster(m, x, y, clusters, medoids)
-				newcost := clusterCost(medoids[m], clusters[m])
+				newcost := clusterCost(medoids[m], clusters[m], f)
 				if newcost >= cost {
 					swapCluster(m, x, y, clusters, medoids)
 				}
--- localize/locavore/locavore.go
+++ localize/locavore/locavore.go
@@ -26,6 +26,22 @@ type CausalEstimator struct {
 	outMedoids []dgtypes.Clusterable
 }
 
+type Individual struct {
+	cov       dgtypes.Clusterable
+	treatment dgtypes.Clusterable
+	outcome   bool // true if pass, false is fail
+}
+
+func (i *Individual) Dissimilar(o dgtypes.Clusterable) float64 {
+	if other, ok := o.(*Individual); ok {
+		return i.treatment.Dissimilar(other.treatment)
+	}
+	panic("Expected another *Individual to be passed to Dissimilar")
+}
+func (i *Individual) String() string {
+	return fmt.Sprintf("{In: %v, Out: %v, Outcome: %v}", i.cov, i.treatment, i.outcome)
+}
+
 func Localize(okf []dgtypes.FuncProfile, failf []dgtypes.FuncProfile, types []dgtypes.Type, numbins int) {
 	for _, okprof := range okf {
 		for _, failprof := range failf {
@@ -38,44 +54,51 @@ func Localize(okf []dgtypes.FuncProfile, failf []dgtypes.FuncProfile, types []dg
 
 func CausalEffect(okf dgtypes.FuncProfile, failf dgtypes.FuncProfile, numbins int) {
 	var ok, fail []dgtypes.Clusterable = make([]dgtypes.Clusterable, 0), make([]dgtypes.Clusterable, 0)
-	var in, out []dgtypes.Clusterable = make([]dgtypes.Clusterable, 0), make([]dgtypes.Clusterable, 0)
-	for _, objprof := range append(okf.In, okf.Out...) {
-		ok = append(ok, dgtypes.Clusterable(objprof))
+	for i := range okf.In {
+		if len(okf.Out) != 0 {
+			ok = append(ok, &Individual{cov: okf.In[i], treatment: okf.Out[i], outcome: true})
+		}
 	}
-	for _, objprof := range append(failf.In, failf.Out...) {
-		fail = append(fail, dgtypes.Clusterable(objprof))
+	for i := range failf.In {
+		if len(failf.Out) != 0 {
+			fail = append(fail, &Individual{cov: failf.In[i], treatment: failf.Out[i], outcome: false})
+		}
 	}
-	for _, objprof := range append(okf.In, failf.In...) {
-		in = append(in, dgtypes.Clusterable(objprof))
-	}
-	for _, objprof := range append(okf.Out, failf.Out...) {
-		out = append(out, dgtypes.Clusterable(objprof))
+	profs := append(ok, fail...)
+	if len(profs) == 0 {
+		log.Printf("Skipping profiles of %v (not enough data)...\n", okf.FuncName)
+		return
 	}
 
-	profs := append(ok, fail...)
 	log.Printf("Clustering profiles of %v...\n", okf.FuncName)
 
 	// Step 1:   Bin the inputs
 	// Step 1.5: Bin the outputs
 	C := CausalEstimator{
-		ok:      ok,
-		fail:    fail,
-		inputs:  in,
-		outputs: out,
-		profs:   profs,
+		ok:    ok,
+		fail:  fail,
+		profs: append(ok, fail...),
 	}
 	C.bin(numbins)
 	// Step 2: {optional} Propensity scoring
 	// Step 3: Matching outputs with different outcomes, based on covariant
 	//	similarity
-	//C.match()
+	C.match()
 
 	// Step 4: ??
 }
 
 func (c *CausalEstimator) bin(numbins int) {
-	c.inBins, c.inMedoids = KMedoids(numbins, c.inputs)
-	c.outBins, c.outMedoids = KMedoids(numbins, c.outputs)
+	c.inBins, c.inMedoids = KMedoidsFunc(numbins, c.profs,
+		func(this dgtypes.Clusterable, other dgtypes.Clusterable) float64 {
+			if obj, ok := this.(*Individual); ok {
+				if o, ok := other.(*Individual); ok {
+					return obj.cov.Dissimilar(o.cov)
+				}
+			}
+			panic("Expected *Individual")
+		})
+	c.outBins, c.outMedoids = KMedoids(numbins, c.profs)
 	fmt.Printf("Some input medoids: %v\n", c.inMedoids)
 	fmt.Printf("Some input clusters: %v\n", c.inBins)
 }
@@ -84,6 +107,7 @@ func (c *CausalEstimator) match() {
 	// Yang - P Score Matching
 	// Computer pairwise Causal Effects for each treatment
 	// TODO : Compute only one triangle of this matrix
+	// TODO : Store this somewhere
 	for treatment1 := range c.outMedoids {
 		for treatment2 := range c.outMedoids {
 			if treatment1 == treatment2 {
@@ -111,28 +135,43 @@ func (c *CausalEstimator) pairwiseMatch(t1, t2 int) float64 {
 func (c *CausalEstimator) outcome(treatment, ind int) float64 {
 	// if c.outBins[treatment][ind] in the fail list, return 0.0;
 	// else return 1.0
-	return 0.0
+	if x, ok := c.outBins[treatment][ind].(*Individual); ok {
+		if x.outcome == false {
+			return 0.0
+		} else {
+			return 1.0
+		}
+	} else {
+		panic("Expected Individual")
+	}
 }
 
 // gets the covariates of the individual referred to by i
 // in treatment level t
 func (c *CausalEstimator) cov(i, t int) dgtypes.Clusterable {
-	panic("Not Implemented")
-	return nil
+	if x, ok := c.outBins[t][i].(*Individual); ok {
+		return x.cov
+	} else {
+		panic("Expected Individual")
+	}
 }
 
 // mCov is the covariate matching function mentioned in Yang
 // it returns the argmin over elements j in tlevel of ||individual - j||
 // in terms of their covariates
 func (c *CausalEstimator) mCov(individual dgtypes.Clusterable, tlevel int) int {
-	min := 2.0
-	matchindex := -1
-	for i := range c.outBins[tlevel] {
-		dist := c.cov(i, tlevel).Dissimilar(individual)
-		if dist < min {
-			min = dist
-			matchindex = i
+	if ind, ok := individual.(*Individual); ok {
+		min := 2.0
+		matchindex := -1
+		for i := range c.outBins[tlevel] {
+			dist := c.cov(i, tlevel).Dissimilar(ind.cov)
+			if dist < min {
+				min = dist
+				matchindex = i
+			}
 		}
+		return matchindex
+	} else {
+		panic("Expected Individual")
 	}
-	return matchindex
 }
