93b6b48ad5194ce004fa3e5cc701eb9100eabc5f
Tim Henderson <tadh@google.com>
2018-08-06 13:54:44 -0700

    Deleted outdated Evaluate function

--- localize/eval/eval.go
+++ localize/eval/eval.go
@@ -1,10 +1,8 @@
 package eval
 
 import (
-	"context"
 	"fmt"
 	"math"
-	"strings"
 
 	"github.com/timtadh/dynagrok/localize/discflo"
 	"github.com/timtadh/dynagrok/localize/fault"
@@ -12,82 +10,6 @@ import (
 	"github.com/timtadh/dynagrok/localize/mine"
 	"github.com/timtadh/dynagrok/localize/mine/opts"
 )
-
-var Chains = map[string][]string{
-	"CBSFL": []string{
-		"Ranked-List",
-		"Spacial-Jumps",
-		"Behavioral-Jumps",
-		"Behavioral+Spacial-Jumps",
-	},
-	"DISCFLO": []string{
-		"DF-Jumps",
-	},
-	"DISCFLO + FP-Filter": []string{
-		"DF-Jumps",
-	},
-	"SBBFL": []string{
-		"SB-List",
-	},
-}
-
-func Evaluate(faults []*fault.Fault, o *discflo.Options, score mine.ScoreFunc, evalName, methodName, scoreName, chainName string, maxStates int, jumpPrs []float64) (EvalResults, error) {
-	m := mine.NewMiner(o.Miner, o.Lattice, score, o.Opts...)
-	switch evalName {
-	case "RankList":
-		var groups [][]ColorScore
-		if methodName == "CBSFL" {
-			groups = CBSFL(&o.Options, score)
-		} else if strings.HasPrefix(methodName, "DISCFLO") {
-			groups = Discflo(o, score)
-		} else {
-			return nil, fmt.Errorf("no localization method named %v for eval method %v", methodName, evalName)
-		}
-		return RankListEval(faults, o.Lattice, methodName, scoreName, groups), nil
-	case "Markov":
-		results := make(EvalResults, 0, 10)
-		for _, jumpPr := range jumpPrs {
-			var colors map[int][]int
-			var P [][]float64
-			jumpChain := chainName
-			if methodName == "CBSFL" {
-				switch chainName {
-				case "Ranked-List":
-					colors, P = RankListMarkovChain(maxStates, m)
-					return HTRank(faults, o.Lattice, methodName, scoreName, chainName, colors, P), nil
-				case "Spacial-Jumps":
-					colors, P = SpacialJumps(jumpPr, maxStates, m)
-					jumpChain = fmt.Sprintf("%v(%g)", chainName, jumpPr)
-				case "Behavioral-Jumps":
-					colors, P = BehavioralJumps(jumpPr, maxStates, m)
-					jumpChain = fmt.Sprintf("%v(%g)", chainName, jumpPr)
-				case "Behavioral+Spacial-Jumps":
-					colors, P = BehavioralAndSpacialJumps(jumpPr, maxStates, m)
-					jumpChain = fmt.Sprintf("%v(%g)", chainName, jumpPr)
-				default:
-					return nil, fmt.Errorf("no chain named %v", methodName)
-				}
-			} else if methodName == "SBBFL" {
-				colors, P = DsgMarkovChain(maxStates, m, m.Mine(context.TODO()).Unique(), 0, nil)
-				return HTRank(faults, o.Lattice, methodName, scoreName, chainName, colors, P), nil
-			} else if strings.HasPrefix(methodName, "DISCFLO") {
-				var err error
-				colors, P, err = DiscfloMarkovChain(jumpPr, maxStates, o, score)
-				if err != nil {
-					return nil, err
-				}
-				jumpChain = fmt.Sprintf("%v(%g)", chainName, jumpPr)
-			} else {
-				return nil, fmt.Errorf("no localization method named %v for eval method %v", methodName, evalName)
-			}
-			r := HTRank(faults, o.Lattice, methodName, scoreName, jumpChain, colors, P)
-			results = append(results, r...)
-		}
-		return results, nil
-	default:
-		return nil, fmt.Errorf("no evaluation method named %v", evalName)
-	}
-}
 
 type ColorScore struct {
 	Color int
